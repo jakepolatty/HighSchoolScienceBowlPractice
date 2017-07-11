312ee677359c8bbbfce1badbbfa4e3179bb35a05
jakepolatty <jakepolatty@gmail.com>
2017-07-11 18:54:06 -0400

    Built basic json parser to load into dictionary

---
+++ High School Science Bowl Practice/QuestionJSONParser.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionJSONParser.swift
+//  High School Science Bowl Practice
+//
+//  Created by Jake Polatty on 7/11/17.
+//  Copyright © 2017 Jake Polatty. All rights reserved.
+//
+
+import Foundation
+
+struct QuestionJSONParser {
+    func parseJsonFile(withName name: String) -> [String: Any]? {
+        let file = Bundle.main.path(forResource: name, ofType: "json")
+        let data = try? Data.init(contentsOf: URL(fileURLWithPath: file!))
+        let jsonData = try? JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+        return jsonData
+    }
+}
